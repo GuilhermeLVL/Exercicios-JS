05150bcd1eaae0a37767d5d0032e66b739b2c849
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2022-12-26 17:31:26 -0300

    feito

--- 25 - Contando de 0 até um número positivo qualquer/jss.js
+++ 25 - Contando de 0 até um número positivo qualquer/jss.js
@@ -1,6 +1,27 @@
-let numPosi = document.getElementById('numPosi');
 
-function clicou (){
-    numPosi
 
-}
+let resposta = document.getElementById(`resposta`);
+
+let enviar = document.getElementById(`enviar`);
+
+enviar.addEventListener(`click`, function(e){
+    e.preventDefault();
+    let numero = document.getElementById(`numero`);
+
+    const value = numero.value;
+
+    for( let i = 0;i<= value;  i++){
+       
+     resposta.innerHTML += `${i}<br>`;
+    }
+    if(value == i){
+        value ==0;
+    }
+
+
+   
+
+
+});
+
+
